8f191328b325c32a88516e3b1585fb028e96b7c6
EugeneZnm <eugenenzioki@gmail.com>
2018-10-18 14:51:32 +0300

    signupforms

---
+++ neighbours/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Profile
+
+from django.contrib.auth.models import User
+
+
+# signup form adding custom field
+class SignUpForm(UserCreationForm):
+    """
+    user creation form for sigup, adding custom field to signup form
+
+    """
+    email = forms.CharField(max_length=254, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+
+
+# Form for editing profile
+class EditProfileForm(forms.ModelForm):
+    """
+    form for editing profile
+    """
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'name', 'neighbourhood']
--- neighbours/views.py
+++ neighbours/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.forms import UserCreationForm
+
+# importing login as auth_login to prevent clashing with inbuilt view
+from django.contrib.auth import login as auth_login
+
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.models import User
+
+from .models import Profile, Neighbourhood, Business
+
+# import forms
+from .forms import SignUpForms
 
 # Create your views here.
